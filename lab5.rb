09cf3b3b60e5f3d7d67f50a3d01ae30b11aeb99a
kyee22@wsl2 <kyee22@buaa.edu.cn>
2024-09-30 20:27:50 +0800

    lab5 a dumb way

---
+++ lab5.rb
@@ -0,0 +1,47 @@
+##############################################################
+##     Ruby Programming @ Beihang University, Fall 2024     ##
+##                Lab5 by Yixuan Kuang                      ##
+##############################################################
+
+keys, values = gets().split(" "), gets().split(" ")
+hash = Hash.new(0)
+keys.each_with_index {|key, index| hash[key] = values[index]}
+puts hash.to_s
+# puts keys.to_s, values.to_s
+
+
+# def twice
+#   yield("Yixuan")
+#   yield("Yixuan")
+# end
+#
+#
+# twice {|x| puts "hello, this is #{x}"}
+#
+# def check_class(&p)
+#   puts p.class
+#   p.call
+#   yield
+# end
+#
+# # Example 1
+# def is_prime(n)
+#   (n.instance_of?(Integer) && n > 1) ? (2..Math.sqrt(n)).all? {|x| n % x > 0} : false
+# end
+#
+#
+# [10,"2024", 0, 1, 2, 3, 4, 5, 997, 2023].each{|x| puts is_prime(x) ? "#{x} is prime!\n" : "#{x} is not prime!\n" }
+#
+#
+# # Example
+# def qsort(lst)
+#   (p = lst.pop) ? qsort(lst.select{|x| x <= p}) + [p] + qsort(lst.select{|x| x > p}) : []
+# end
+#
+# a = [2, 0, 2, 4, 2, 2, 3, 7, 1, 0, 9, 2]
+# puts a.inject(Hash.new(0)) {|hash, x| hash[x] += 1; hash}.inspect
+#
+#
+# check_class {puts "hello, this is Yixuan Kuang!"}
+#
+# puts qsort([42, 7, 5, 19, 88, 5, 62, 14, 91, 33, 75, 91]).inspect
