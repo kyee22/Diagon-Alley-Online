c370665163c9e2004d6174aaf0789802f7ead32a
kyee22@wsl2 <kyee22@buaa.edu.cn>
2024-10-18 00:39:57 +0800

    🚀 feat: category, nav

--- cookbook3/db/schema.rb
+++ cookbook3/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_17_143844) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_17_152945) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_recipes_on_category_id"
   end
+
+  add_foreign_key "recipes", "categories"
 end
