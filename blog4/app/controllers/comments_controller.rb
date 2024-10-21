536420aad378f4750395f08e66b28b9b1c1ba645
kyee22@wsl2 <kyee22@buaa.edu.cn>
2024-10-22 00:56:30 +0800

    🔧 fix: unit test for comment

--- blog4/app/controllers/comments_controller.rb
+++ blog4/app/controllers/comments_controller.rb
@@ -3,20 +3,24 @@ class CommentsController < ApplicationController
 
   # GET /comments or /comments.json
   def index
+    @blog = Blog.find(params[:blog_id])
     @comments = Comment.all
   end
 
   # GET /comments/1 or /comments/1.json
   def show
+    @blog = Blog.find(params[:blog_id])
   end
 
   # GET /comments/new
   def new
+    @blog = Blog.find(params[:blog_id])
     @comment = Comment.new
   end
 
   # GET /comments/1/edit
   def edit
+    @blog = Blog.find(params[:blog_id])
   end
 
   # POST /comments or /comments.json
@@ -39,9 +43,10 @@ class CommentsController < ApplicationController
 
   # PATCH/PUT /comments/1 or /comments/1.json
   def update
+    @blog = Blog.find(params[:blog_id])
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: "Comment was successfully updated." }
+        format.html { redirect_to blog_comment_path(@blog, @comment), notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,10 +57,11 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1 or /comments/1.json
   def destroy
+    @blog = Blog.find(params[:blog_id])
     @comment.destroy!
 
     respond_to do |format|
-      format.html { redirect_to comments_path, status: :see_other, notice: "Comment was successfully destroyed." }
+      format.html { redirect_to blog_comments_path(@blog), status: :see_other, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
     end
   end
--- blog4/test/controllers/comments_controller_test.rb
+++ blog4/test/controllers/comments_controller_test.rb
@@ -2,47 +2,51 @@ require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @blog = blogs(:one)
     @comment = comments(:one)
   end
 
   test "should get index" do
-    get blog_comments_url(@comment.blog)
+    get blog_comments_url(@blog)
     assert_response :success
   end
 
   test "should get new" do
-    get new_comment_url
+    get new_blog_comment_url(@blog)
     assert_response :success
   end
 
   test "should create comment" do
     assert_difference("Comment.count") do
-      post comments_url, params: { comment: { blog_id: @comment.blog_id, contont: @comment.contont } }
+      post blog_comments_url(@blog), params: { comment: { blog_id: @comment.blog_id, contont: @comment.contont } }
     end
 
-    assert_redirected_to comment_url(Comment.last)
+    # assert_redirected_to comment_url(Comment.last)
+    # assert_redirected_to blog_comment_url(@blog, Comment.last)
+    assert_redirected_to blog_url(@blog)
   end
 
   test "should show comment" do
-    get comment_url(@comment)
+    # get comment_url(@comment)
+    get blog_comment_url(@blog, @comment)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_comment_url(@comment)
+    get edit_blog_comment_url(@blog, @comment)
     assert_response :success
   end
 
   test "should update comment" do
-    patch comment_url(@comment), params: { comment: { blog_id: @comment.blog_id, contont: @comment.contont } }
-    assert_redirected_to comment_url(@comment)
+    patch blog_comment_url(@blog, @comment), params: { comment: { blog_id: @comment.blog_id, contont: @comment.contont } }
+    assert_redirected_to blog_comment_url(@blog, @comment)
   end
 
   test "should destroy comment" do
     assert_difference("Comment.count", -1) do
-      delete comment_url(@comment)
+      delete blog_comment_url(@blog, @comment)
     end
 
-    assert_redirected_to comments_url
+    assert_redirected_to blog_comments_url(@blog)
   end
 end
