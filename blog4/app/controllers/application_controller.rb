81a4a5a7021d083fae5f82eaf99241caad636629
kyee22@wsl2 <kyee22@buaa.edu.cn>
2024-12-05 18:38:48 +0800

    🚀 feat: add 2 layout

--- blog4/app/controllers/application_controller.rb
+++ blog4/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  layout :choose_layout
+
+  private
+    def choose_layout
+      if controller_name == "welcome"
+        "home"
+      else
+        "application"
+      end
+    end
+
 end
