cb5406550421f13c7490707ccfad1946dce9c405
kyee22@wsl2 <kyee22@buaa.edu.cn>
2024-12-15 04:48:14 +0800

    透明度 -> 不透明度

--- project/app/models/color.rb
+++ project/app/models/color.rb
@@ -5,7 +5,7 @@ class Color < ApplicationRecord
   # 确保代码存在且格式正确
   validates :code, presence: true, format: { with: /\A#[0-9A-Fa-f]{6}\z/, message: "must be a valid hex color code" }
 
-  # 确保透明度在 0 到 1 之间
+  # 确保不透明度在 0 到 1 之间
   validates :opacity, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 
   has_many :products
